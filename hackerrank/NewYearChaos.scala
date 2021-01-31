dd1e9122f72984c2c492e46c5b4adcbe0dd54517
Bibin Nahas <bbnnaz@gmail.com>
2021-01-31 13:49:56 +0530

    alternate Commit for new year chaos problem

--- hackerrank/NewYearChaos.scala
+++ hackerrank/NewYearChaos.scala
@@ -1,5 +1,7 @@
 package hackerrank
 
+import scala.annotation.tailrec
+
 object NewYearChaos {
 
   def rotateArray(A: Array[Int], K: Int): Array[Int] = {
@@ -20,56 +22,30 @@ object NewYearChaos {
     var tempArray = Array[Int]()
     tempArray = q
 
+    @tailrec
     def helperMethod(q: Array[Int], indexVal: Int, temp: Int, acc: Int): Int = {
-      //      println(tempArray.toList)
-      if (indexVal == size ) {
-//        println("in ZERO")
-        acc
-      }
-      else if ((q.indexOf(indexVal) == temp)) {
-//        println("in ONE")
-//        println("q: "+q.toList)
-//        println(q.tail.toList)
-//        println("indexVal: " + indexVal)
-//        println("acc: " + acc)
+      if (indexVal == size) acc
+      else if ((q.indexOf(indexVal) == temp))
         helperMethod(q.tail, indexVal + 1, 0, acc)
-      }
       else if ((q.indexOf(indexVal) == temp + 2)) {
-//        println("in TWO")
-//        println("q: "+q.toList)
         tempArray = q
         var tempVariable = q(0)
         var secTempVariable = q(1)
         tempArray(0) = indexVal
         tempArray(1) = tempVariable
         tempArray(2) = secTempVariable
-//        println(tempArray.tail.toList)
-//        println("indexVal: " + indexVal)
-//        println("acc: " + (acc + 2))
         helperMethod(tempArray.tail, indexVal + 1, 0, acc + 2)
       }
       else if ((q.indexOf(indexVal) == temp + 1)) {
-//        println("in THREE")
-//        println("q: "+q.toList)
         tempArray = q
         var tempVariable = q(0)
         var secTempVariable = q(1)
         tempArray(0) = secTempVariable
         tempArray(1) = tempVariable
-//        println(tempArray.toList)
-//        println("indexVal: " + indexVal)
-//        println("acc: " + (acc + 1))
         helperMethod(tempArray.tail, indexVal + 1, 0, acc + 1)
       }
-      else {
-//        println("in FOUR")
-//        println("indexVal: " + indexVal)
-//        println("q: "+q.toList)
-//        println("new q: "+rotateArray(q, q.size - q.indexOf(indexVal)).toList)
-//        println("val: "+ q.indexOf(indexVal))
-//        println("acc: " + (acc + 1))
+      else
         helperMethod(rotateArray(q, q.size - q.indexOf(indexVal)).tail, indexVal + 1, 0, acc + q.indexOf(indexVal))
-      }
     }
 
     helperMethod(q, (q.size - (q.size - 1)), 0, 0)
